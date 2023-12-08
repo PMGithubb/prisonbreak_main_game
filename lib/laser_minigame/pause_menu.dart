d154bc91a7b2268194fd4095dd082b35a2d33389
Pournima <ponna.mhaskar@gmail.com>
2023-12-08 18:04:35 -0500

    Restart button added to pause menu.

--- lib/laser_minigame/pause_menu.dart
+++ lib/laser_minigame/pause_menu.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:prisonbreak_main_game/laser_minigame/gameover_menu.dart';
 import 'package:prisonbreak_main_game/laser_minigame/laser_game.dart';
+import 'package:prisonbreak_main_game/laser_minigame/main_menu.dart';
 import 'package:prisonbreak_main_game/laser_minigame/pause_button.dart';
 
 class PauseMenu extends StatelessWidget {
@@ -35,25 +37,58 @@ class PauseMenu extends StatelessWidget {
                         color: Color.fromARGB(255, 255, 255, 255)),
                   ),
                 ),
-                Padding(
-                  padding: const EdgeInsets.only(bottom: 20),
-                  child: ElevatedButton(
-                      onPressed: () {
-                        gameRef.resumeEngine();
-                        gameRef.overlays.remove(PauseMenu.ID);
-                        gameRef.overlays.add(PauseButton.ID);
-                      },
-                      style: ElevatedButton.styleFrom(
-                          shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(
-                                200.0), // Adjust the border radius
-                          ),
-                          fixedSize: const Size(130, 130),
-                          foregroundColor: const Color.fromARGB(0, 0, 0, 0),
-                          shadowColor: const Color.fromARGB(0, 0, 0, 0),
-                          backgroundColor: const Color.fromARGB(0, 0, 0, 0)),
-                      child: Image.asset(
-                          "assets/images/square_buttons/PlaySquareButton.png")),
+                Center(
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.only(bottom: 20),
+                        child: ElevatedButton(
+                            onPressed: () {
+                              gameRef.resumeEngine();
+                              gameRef.overlays.remove(PauseMenu.ID);
+                              gameRef.overlays.add(PauseButton.ID);
+                            },
+                            style: ElevatedButton.styleFrom(
+                                shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(
+                                      200.0), // Adjust the border radius
+                                ),
+                                fixedSize: const Size(130, 130),
+                                foregroundColor:
+                                    const Color.fromARGB(0, 0, 0, 0),
+                                shadowColor: const Color.fromARGB(0, 0, 0, 0),
+                                backgroundColor:
+                                    const Color.fromARGB(0, 0, 0, 0)),
+                            child: Image.asset(
+                                "assets/images/square_buttons/PlaySquareButton.png")),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(bottom: 10),
+                        child: ElevatedButton(
+                            onPressed: () {
+                              gameRef.overlays.remove(GameOverMenu.ID);
+                              Navigator.of(context)
+                                  .pushReplacement(MaterialPageRoute(
+                                builder: (context) => const MainMenu(),
+                              ));
+                            },
+                            style: ElevatedButton.styleFrom(
+                                shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(
+                                      200.0), // Adjust the border radius
+                                ),
+                                fixedSize: const Size(130, 130),
+                                foregroundColor:
+                                    const Color.fromARGB(0, 0, 0, 0),
+                                shadowColor: const Color.fromARGB(0, 0, 0, 0),
+                                backgroundColor:
+                                    const Color.fromARGB(0, 0, 0, 0)),
+                            child: Image.asset(
+                                "assets/images/square_buttons/ReturnSquareButton.png")),
+                      ),
+                    ],
+                  ),
                 )
               ],
             ),
