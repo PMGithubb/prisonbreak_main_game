fc025c05091775887b17e25cc906ac1444c3cdec
Anwita <anwitasri@gmail.com>
2023-12-07 23:04:02 -0500

    Finished Mvp

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,7 @@ import 'package:flutter/widgets.dart';
 void main() => runApp(
     MaterialApp(home: MyApp(),
     theme: new ThemeData(scaffoldBackgroundColor: const Color(0xFF5C339B))));
+
 class MyApp extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
@@ -36,27 +37,44 @@ class MyApp extends StatelessWidget{
         ),
       ),
     ),
-
+    Positioned(
+      right: 281,
+      top: 50,
+      child: Text(
+        "Prison Escape",
+        style: TextStyle(fontSize: 50, color: Colors.white)
+      )
+    )
     ],
     ),
     );
   }
 }
 
+var points = 0;
 
 class OpeningScreen extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
+    var screenSize = MediaQuery.of(context).size;
+    var width = screenSize.width;
+    var height = screenSize.height;
     return Scaffold(
     body: Stack(
       children:[
        Center(
-        child: Image.asset("assets/possible_background.png")
-      ),
-        Center(
+         child:FittedBox(
+           child: Image.asset("assets/possible_background.png",
+             width: width,
+            height: height)
+    ),
+         ),
+
+        Align(
+          alignment: Alignment.bottomRight,
           child: SizedBox(
-            width: 200,
-            height: 200,
+            width: 125,
+            height: 125,
               child: GestureDetector(
                 onTap: (){
                   Navigator.push(
@@ -67,6 +85,47 @@ class OpeningScreen extends StatelessWidget{
                 )
               )
           )
+        ),
+         Positioned(
+          right: 560,
+          top: 139,
+          child: Image.asset("assets/images/sprite_final.png",
+          height: 200,
+          width: 200,)
+        ),
+        Positioned(
+          right:500,
+          top: 20,
+            child: Image.asset("assets/bubble2.png"),
+            height:200,
+          width:200
+        ),
+        Positioned(
+          right: 100,
+          top: 100,
+          child: Container(
+            color: Colors.black.withOpacity(0.5),
+            padding: EdgeInsets.all(5),
+            width: 400,
+            child: Expanded(
+            child: Text(
+              "In this game, there are 2 levels. The first level has a question and "
+                  "answer section, as well as a minigame. The second section "
+                  "has a harder question and answer section compared to the first level. "
+                  "In each level, the goal is to obtain as many points as possible "
+                  "by answering questions correctly, and playing the minigame. A minimum "
+                  "of 100 points must be achieved to successfully obtain the key to "
+                  "escape the prison.",
+              style: TextStyle(fontSize: 15, color: Colors.white),
+              softWrap: true,
+              overflow: TextOverflow.visible,
+            ))
+          )
+          ),
+        Positioned(
+          right:535,
+          top:100,
+          child: Text("I need to get that key...")
         )
     ]
     ),
@@ -76,13 +135,64 @@ class OpeningScreen extends StatelessWidget{
 class successClosingScreen extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
+    var screenSize = MediaQuery.of(context).size;
+    var width = screenSize.width;
+    var height = screenSize.height;
     return Scaffold(
       body: Stack(
         children:[
           Center(
-            child: Image.asset("assets/images/escape_success.jpg",)
+              child: Image.asset(
+                  "assets/images/escape_success.jpg"
+              )),
+
+            Align(
+              alignment: Alignment.bottomRight,
+              child: SizedBox(
+               width: 130,
+               height: 130,
+               child: GestureDetector(
+                onTap: (){
+                 Navigator.push(
+    context, MaterialPageRoute(builder: (context)=> failureClosingScreen()));
+                      },
+                  child: Image.asset(
+                    "assets/images/Square Buttons/Next Square Button.png",
+                  ),
+              )
+            )
+          ),
+          Positioned(
+            right: 310,
+              top: 150,
+              child: SizedBox(
+                  width: 130,
+                  height: 130,
+                  child: GestureDetector(
+                      onTap: () {
+                        Navigator.push(
+                            context, MaterialPageRoute(builder: (context) => MyApp()));
+                      },
+    child: Image.asset(
+    "assets/images/Square Buttons/Return Square Button.png"
+    ))
+              )),
+          Align(
+            alignment: Alignment.topLeft,
+            child: Text(
+              style: TextStyle(fontSize: 50, color: Colors.white),
+              "Points: ${points}"
+            )
+          ),
+          Positioned(
+            right:200,
+            top:50,
+            child: Text(
+              "You escaped the prison!",
+              style: TextStyle(fontSize: 50, color: Colors.white)
+            )
           )
-        ]
+       ]
       ),
     );
   }
@@ -93,8 +203,29 @@ class failureClosingScreen extends StatelessWidget{
     return Scaffold(
       body: Stack(
         children:[
-          Center(
-            child: Text("Try Again.."),
+          Positioned(
+            right:400,
+            top: 50,
+            child: Text(
+                "Try Again..",
+                 style: TextStyle(fontSize: 50)),
+          ),
+     Center(
+        child: SizedBox(
+     width: 130,
+       height: 130,
+         child: GestureDetector(
+          onTap: () {
+       Navigator.push(
+       context, MaterialPageRoute(builder: (context) => MyApp()));
+    },
+         child: Image.asset("assets/images/Square Buttons/Return Square Button.png")))),
+          Align(
+            alignment: Alignment.topLeft,
+            child: Text(
+              style: TextStyle(fontSize: 50, color: Colors.white),
+              "Points: ${points}"
+            )
           )
         ]
       ),
