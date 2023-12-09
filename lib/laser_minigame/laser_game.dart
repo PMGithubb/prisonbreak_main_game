d5b2dc930f3f977623683c3cf98be62cd53b9492
Pournima <ponna.mhaskar@gmail.com>
2023-12-08 19:05:08 -0500

    Timer countdown added Score set to zero if timer runs out

--- lib/laser_minigame/laser_game.dart
+++ lib/laser_minigame/laser_game.dart
@@ -3,8 +3,6 @@ import 'package:flame/game.dart';
 import 'package:prisonbreak_main_game/laser_minigame/gameover_menu.dart';
 import 'package:prisonbreak_main_game/laser_minigame/pause_button.dart';
 import 'package:shared_preferences/shared_preferences.dart';
-//import 'package:prisonbreak_main_game/pause_menu.dart';
-//import 'package:prisonbreak_main_game/laser_minigame/player_button.dart';
 import 'player.dart';
 import 'laser.dart';
 import 'background.dart';
@@ -14,36 +12,63 @@ class LaserGame extends FlameGame with HasCollisionDetection {
   late Player player;
   late Laser laser;
   late TextComponent scoreText;
+  late TextComponent timeText;
   int score = 100;
   late final SharedPreferences prefs;
+
+  late Timer countDown;
+  int remainingTime = 20;
+  bool timerStarted = false;
 
   @override
   Future<void> onLoad() async {
     add(background = Background(sprite: await loadSprite('prison_yard.png')));
     add(player = Player(sprite: await loadSprite('sprite_final.png')));
     add(laser = Laser(sprite: await loadSprite('laser.png')));
-
     score = 100;
     prefs = await SharedPreferences.getInstance();
     //prefs.setInt("highScore", 0);
     scoreText = TextComponent(position: Vector2(650, 40));
+    timeText = TextComponent(position: Vector2(20, 40));
     add(scoreText);
+    add(timeText);
+    countDown = Timer(1, onTick: () {
+      if (remainingTime > 0) {
+        remainingTime -= 1;
+      }
+    }, repeat: true);
+    timerStarted = true;
   }
 
   @override
   void update(double dt) {
     super.update(dt);
     scoreText.text = 'Score: $score';
-    if (player.x >= 800) {
+    timeText.text = 'Time remaining: $remainingTime secs';
+    if (remainingTime <= 0) {
+      score = 0;
+      scoreText.removeFromParent();
       player.removeFromParent();
       player.x = 0;
       overlays.remove(PauseButton.ID);
       overlays.add(GameOverMenu.ID);
     }
+    if (player.x >= 780) {
+      timeText.removeFromParent();
+      scoreText.removeFromParent();
+      player.removeFromParent();
+      player.x = 0;
+      overlays.remove(PauseButton.ID);
+      overlays.add(GameOverMenu.ID);
+    }
+
+    if (timerStarted && remainingTime > 0) {
+      countDown.update(dt);
+    }
   }
 
   void decreaseScore() {
-    score -= 20;
+    score -= 10;
   }
 
   String getScore() {
